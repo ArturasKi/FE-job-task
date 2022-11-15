33ddf10fe466319a1c1e27d5d6f47335056f910b
Arturito <arturaskisielius91@gmail.com>
2022-11-15 10:55:13 +0200

    Delete function

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -4,30 +4,29 @@ import Categories from "./Components/Categories";
 import taskData from "./js-task.json";
 
 export default function App() {
-
-  const [data, setData] = useState(taskData)
-
-  let dataArr = [data];
-  console.log(dataArr)
+  const [data, setData] = useState(taskData);
 
   const deleteHandler = (id) => {
-    console.log(id)
-    // setData(current => current.children.filter(item => item.id !== id));
-    function recursiveRemove ( list, id ) {
-      return list.map ( item => { return {...item} }).filter ( item => {
-          if ( 'children' in item ) {
-              item.children = recursiveRemove ( item.children, id );
+    function recursiveRemove(list, id) {
+      return list
+        .map((item) => {
+          return { ...item };
+        })
+        .filter((item) => {
+          if ("children" in item) {
+            item.children = recursiveRemove(item.children, id);
           }
           return item.id !== id;
-      });
-  }
-  console.log(recursiveRemove(dataArr, id));
-  }
-  
+        });
+    }
+    const removedElement = recursiveRemove([data], id);
+    setData(...removedElement, data);
+  };
+
   return (
     <div className="container">
       <h1>Categories tree</h1>
-      <Categories data={data} deleteHandler={deleteHandler}/>
+      <Categories data={data} deleteHandler={deleteHandler} />
     </div>
   );
 }
--- r1/src/Components/Categories.jsx
+++ r1/src/Components/Categories.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
-export default function Categories({data, deleteHandler}) {
+export default function Categories({ data, deleteHandler }) {
   const [isVisible, setIsVisible] = useState(false);
+
   const expand = () => {
     setIsVisible(!isVisible);
   };
@@ -9,12 +10,18 @@ export default function Categories({data, deleteHandler}) {
   return (
     <div draggable className="category-item">
       <span onClick={expand}>{data.name}</span>
-      <button onClick={() => deleteHandler(data.id)} className="btn">Delete</button>
+      <button onClick={() => deleteHandler(data.id)} className="btn">
+        Delete
+      </button>
       {isVisible ? (
-        data.children.map((child, key) => {
+        data?.children?.map((child, key) => {
           return (
             <div key={key} draggable className="category-item">
-              <Categories key={child.id} data={child} deleteHandler={deleteHandler}/>
+              <Categories
+                key={child.id}
+                data={child}
+                deleteHandler={deleteHandler}
+              />
             </div>
           );
         })
